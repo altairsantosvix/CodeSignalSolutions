7aae208d21df5bd1832810c9db5cefe0071a67b0
altairsantosvix <altairdosantosu@gmail.com>
2021-08-29 10:08:40 -0300

    step3

---
+++ Arcade Universe/Intro/The Journey Begins/Go/CheckPalindrome.go
@@ -0,0 +1,10 @@
+
+func checkPalindrome(inputString string) bool {
+	var lastI = len(inputString) - 1
+	for i := range inputString {
+		if inputString[i] != inputString[lastI] {
+			return false
+		}
+		lastI--
+	}
+	return true
